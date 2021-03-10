d65aa431e7dbbc5a0b496e8cc777bcf979a0ac9d
Andreyz11288 <andreyz11288@gmail.com>
2021-03-10 19:42:33 +0200

    пофиксил вертикальную прокрутку

--- src/Page/HomePage/HomePage.js
+++ src/Page/HomePage/HomePage.js
@@ -2,7 +2,6 @@ import { useSelector } from 'react-redux';
 import { NavLink } from 'react-router-dom';
 import { getIsAutheticated } from '../../Redux/Auth/authSelectors';
 import s from './HomePage.module.scss';
-// import Hotels from '../../Components/Hotels/Hotels';
 
 const HomePage = () => {
   const isLogin = useSelector(getIsAutheticated);
@@ -10,25 +9,29 @@ const HomePage = () => {
     <section>
       <div className={s.bacground}></div>
       <div className={s.overley}></div>
-      <p className={s.smallText}>
-        ENJOY YOUR WONDERFUL HOLIDAYS WITH A GREAT LUXURY EXPERIENCE!
-      </p>
-      <h1 className={s.heroText}>Most Relaxing Place</h1>
-      {!isLogin && (
-        <div className={s.homeLinck}>
-          <p className={s.linckLogoReg}>
-            Please &nbsp;
-            <NavLink className={s.HomePageLogo} to="/login">
-              login
-            </NavLink>
-            &nbsp; or &nbsp;
-            <NavLink className={s.HomePageReg} to="/register">
-              register
-            </NavLink>
-            .
-          </p>
-        </div>
-      )}
+
+      <div className={s.heroBlockText}>
+        <p className={s.smallText}>
+          ENJOY YOUR WONDERFUL HOLIDAYS WITH A GREAT LUXURY
+          EXPERIENCE!
+        </p>
+        <h1 className={s.heroText}>Most Relaxing Place</h1>
+        {!isLogin && (
+          <div className={s.homeLinck}>
+            <p className={s.linckLogoReg}>
+              Please &nbsp;
+              <NavLink className={s.HomePageLogo} to="/login">
+                login
+              </NavLink>
+              &nbsp; or &nbsp;
+              <NavLink className={s.HomePageReg} to="/register">
+                register
+              </NavLink>
+              .
+            </p>
+          </div>
+        )}
+      </div>
     </section>
   );
 };
