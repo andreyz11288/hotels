323973d2fc5b7809f003efadebf7812bec0b6c60
Andreyz11288 <andreyz11288@gmail.com>
2021-03-15 23:09:36 +0200

    добавил скрол

--- src/Page/Apartments/Apartments.js
+++ src/Page/Apartments/Apartments.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Hotels from '../../Components/Hotels/Hotels';
+import ScrollUp from '../../Components/ScrollUp';
 
 export default function Apartments() {
   return (
     <>
       <Hotels />
+      <ScrollUp />
     </>
   );
 }
