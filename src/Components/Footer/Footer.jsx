57f588098b3bcdbc9aaad13ae9290dc3a18504c4
galka-travinskaya <66030379+galka-travinskaya@users.noreply.github.com>
2021-03-31 10:54:41 +0300

    fix

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,22 +1,32 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
+import { getIsAutheticated } from '../../Redux/Auth/authSelectors';
 import s from './Footer.module.scss';
 
 export default function Footer() {
+  const isLoggedIn = useSelector(getIsAutheticated);
+
   return (
-    <div className={s.footer}>
-      <div className={s.textContent}>
-        <p className={s.content}>
-          Want to keep up with the best deals?
-        </p>
-      </div>
-      <div className={s.subscription}>
-        <input
-          type="email"
-          placeholder="Email"
-          className={s.entryField}
-        />
-        <button type="submit" className={s.subscribeButton}>Subscribe</button>
-      </div>
-    </div>
+    <>
+      {isLoggedIn && (
+        <div className={s.footer}>
+          <div className={s.textContent}>
+            <p className={s.content}>
+              Want to keep up with the best deals?
+            </p>
+          </div>
+          <div className={s.subscription}>
+            <input
+              type="email"
+              placeholder="Email"
+              className={s.entryField}
+            />
+            <button type="submit" className={s.subscribeButton}>
+              Subscribe
+            </button>
+          </div>
+        </div>
+      )}
+    </>
   );
 }
